cc4ba0006a90563b5413d4606774b9eabb53c4fd
YIlun Weng <yilunweng@YIluns-iMac-Pro.local>
2019-03-29 10:14:52 -0700

    Carousel added - some minor fix needed

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -4,6 +4,7 @@ import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+import Carousel from "../Carousel/Carousel";
 
 export default class Content extends Component {
   constructor(props) {
@@ -46,6 +47,7 @@ export default class Content extends Component {
         <Tabs tabs={this.state.tabs}
               selectTabHandler={this.changeSelected}
               selectedTab={this.state.selected}/>
+        <Carousel/>
         <Cards cards={this.filterCards()} />
       </div>
     );
